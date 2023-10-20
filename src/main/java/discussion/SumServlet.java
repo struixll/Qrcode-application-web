ac90e28a88cf7fcfbe2da10495a1349cb2aada17
struixll <ileona-barbosa@hotmail.fr>
2023-10-20 11:41:28 +0200

    Rajout du message lors d'un get

--- src/main/java/discussion/SumServlet.java
+++ src/main/java/discussion/SumServlet.java
@@ -2,6 +2,8 @@ package discussion;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -33,11 +35,8 @@ public class SumServlet extends HttpServlet {
 
         try {
             BitmapCanvasProvider canvas = new BitmapCanvasProvider(dpi, BufferedImage.TYPE_BYTE_BINARY, false, 0);
-
-            // Inclure le message dans le code-barre
             bean.generateBarcode(canvas, message);
             canvas.finish();
-
             response.setContentType("image/png");
             ServletOutputStream out = response.getOutputStream();
             ImageIO.write(canvas.getBufferedImage(), "png", out);
@@ -53,7 +52,10 @@ public class SumServlet extends HttpServlet {
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("Bienvenue");
     }
 }
 
